67b22438400fc8900b53da21eb4ede3d19378e9d
Akshaj Rao <akshajrao2004@gmail.com>
2024-09-09 01:55:56 +0530

    feat(euler): add new euler method

---
+++ Forward Euler method/euler_method.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+/**
+ * Derivative
+ *
+ * Code implementation for any real-valued derivative
+ *
+ * @param x (long double) The first argument of the function.
+ * @param y (long double) The second argument of the function.
+ * @return (long double) The value of the function.
+ */
+long double y_prime(long double t, long double y) { return (t + y + t * y); }
+
+int main() {
+  long double t0, y0, h, t1;
+
+  std::cout << "Enter the initial value (t0): ";
+  std::cin >> t0;
+
+  std::cout << "Enter the value of the function at this point (y(t0)): ";
+  std::cin >> y0;
+
+  std::cout << "Enter the value of the step size (h): ";
+  std::cin >> h;
+
+  std::cout << "Enter the value at which you need to approximate y: ";
+  std::cin >> t1;
+
+  while (t0 < t1) {
+    y0 += h * y_prime(t0, y0);
+    t0 += h;
+  }
+
+  std::cout << "Approximate solution at x = " << t1 << " is " << y0
+            << std::endl;
+
+  return 0;
+}
